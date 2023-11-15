19606c222ed983aae5738ab4fadcc87cffe309c3
StasenkO1337 <78676050+StasenkO1337@users.noreply.github.com>
2023-11-15 21:37:46 +0500

    Update UserServiceImpl.java
    и тут убрал

--- src/main/java/sprint/test/service/UserServiceImpl.java
+++ src/main/java/sprint/test/service/UserServiceImpl.java
@@ -20,7 +20,6 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
-    @Transactional
     @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
